ae939306d1383fe07bab5bfd2f71cccfd22eb789
redaalch <s133260695@taalim.ma>
2025-09-27 20:14:31 +0100

    first commit

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,12 @@
+function Food() {
+    const food1 = "orange";
+    const food2 = "Banana";
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+export default Food
