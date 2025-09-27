f6ad4b9b64f57bb00a7f57f4afb8d58a81d5411b
redaalch <s133260695@taalim.ma>
2025-09-27 22:29:39 +0100

    render array of objects

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,19 @@
+function List(props) {
+  // fruits.sort((a,b)=>a.name.localeCompare(b.name));//alpha
+
+  // const lowCalFruits = fruits.filter(fruit => fruit.calories<100); filter
+  const category = props.category;
+  const itemList = props.items;
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; <b>{item.calories}</b>
+    </li>
+  ));
+  return (
+    <div>
+      <h3 className="list-category">{category}</h3>
+      <ul className="list-items">{listItems}</ul>
+    </div>
+  );
+}
+export default List;
