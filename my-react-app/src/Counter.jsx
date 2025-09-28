4b05df2426dc8ebc3abbd896d062a43349d217c0
redaalch <s133260695@taalim.ma>
2025-09-28 19:44:50 +0100

    counter app

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -1,8 +1,30 @@
-import React,{useState} from "react"
-
+import React, { useState } from "react";
 
 function Counter() {
-    
-    return
+  const [count, setCount] = useState(0);
+  const increament = () => {
+    setCount(count + 1);
+  };
+  const decreament = () => {
+    setCount(count - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter-container">
+      <h1 className="count-display">{count}</h1>
+      <button className="counter-button" onClick={increament}>
+        increament
+      </button>
+      <button className="counter-button" onClick={reset}>
+        reset
+      </button>
+      <button className="counter-button" onClick={decreament}>
+        decreament
+      </button>
+    </div>
+  );
 }
-export default Counter
+export default Counter;
